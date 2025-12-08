16c86b6ec093846350e9498997232513bbca5fcc
GenopticSmartDisplays <ryan@genopticsmartdisplays.com>
2025-12-08 14:58:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/1KOGu6rBKsWDouj-yNwoSpznNX42tA90tWBrAGzGhmqzAQYZ5i7j0iKzB/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyo0wwgbYdWvE9o5JZln90P882BNEIH1WaQD7L5pXtn0Tj1Qo_ea8zrWXpSgzf62kpxFw/exec';
