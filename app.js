d10b879c5bb2835abfeffad49a28f4ff7b6a936e
GenopticSmartDisplays <ryan@genopticsmartdisplays.com>
2025-12-09 10:49:56 -0700

    Added Cloudflare Proxy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ==================== CONFIGURATION ====================
 const CONFIG = {
     // API Configuration
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzM80Bmd-BbKFgu9k-u8_SPD4MukDfqehPCKcdudU0bS1rZs57thFcaz7qQK0DXO0bMVw/exec',
+    GOOGLE_SCRIPT_URL: 'https://sign-calculator-proxy.genoptic2025.workers.dev',
     
     // API Security (Update these with your actual keys)
     API_KEY: 'HS_QUOTE_CALC_2024_SECURE_KEY_XYZ123',
